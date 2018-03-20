60b7435cf68be34c11f1a0e28b799a0a36303de9
Neville Mehta <nevmehta@ebay.com>
2018-03-20 00:06:17 -0700

    load async package when counter reached 10

--- src/components/fe/my-counter/component.js
+++ src/components/fe/my-counter/component.js
@@ -8,7 +8,7 @@ module.exports = {
     
     // get widget config would return this!
     // now, we just set it on 'this'
-    this.delta = 5;
+    this.delta = 1;
 
     // create empty state!
     this.state = {
@@ -22,24 +22,47 @@ module.exports = {
     // this is like the init()
     // remember, stuff passed from the server side is simply available on 'this'
     this.state = {
-      value: this.input.value ? this.input.value : 0
+      value: this.input.value ? this.input.value : 0,
+      myButton: null
     }
+  },
+
+  checkNameValidator() {
+    var myButton = null;
+    if ( this.state.value === 10 ) {
+      myButton = require('../my-button')
+        .renderSync({buttonName: 'Load async name validator'})
+        .appendTo(document.getElementById('my-client-button')) // change so that we can continue to use the sticky header without the transform issue (SRP non-breaking)
+        .getComponent();
+    } else {
+      if ( this.state.myButton ) {
+        this.state.myButton.destroy();
+      }
+    }
+    this.setState({
+      myButton: myButton,
+      value: this.state.value
+    });    
   },
 
   // add
   add(event, el) {
     console.log("add");
     this.setState({
-      value: this.state.value + this.delta
+      value: this.state.value + this.delta,
+      myButton: this.state.myButton
     })
+    this.checkNameValidator();
   },
 
   // minus!
   minus() {
     console.log("minus");
     this.setState({
-      value: this.state.value - this.delta
+      value: this.state.value - this.delta,
+      myButton: this.state.myButton
     })
+    this.checkNameValidator();
   }
 
 };
--- src/components/fe/my-hello/index.js
+++ src/components/fe/my-hello/index.js
@@ -1,4 +1,4 @@
 
 module.exports.render = function(input, out) {
-	out.write('<h1>Hello ' + input.name + '</h1>')
+	out.write('<h1>Hello ' + input.name + ' (10 is the magic number!)</h1>')
 }
